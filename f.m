e92850956017ebcca3348bd3ff317bbc8e6789f7
Lucas <lucas.gamaleri.92@gmail.com>
2018-09-21 22:41:50 -0300

    First Commit

---
+++ f.m
@@ -0,0 +1,67 @@
+function [xp] = f(t,y)
+%y = [ uo vo wo 0 0 0 0 titacero ];
+global rho 
+global vinf
+global sup
+global b
+global c
+global g
+global m
+global de
+global Ix
+global Iy
+global Iz
+global V
+
+V = sqrt(y(1)^2 + y(2)^2 + y(3)^2) ;
+betha = asind(y(2)/vinf);
+alpha = atand(y(3)/y(1));
+
+
+fila1 = [1 alpha alpha^2 alpha^3 alpha^4 alpha^5 alpha^6 abs(betha) betha^2 abs(betha^3) betha^4 alpha*abs(betha)]; 
+
+fila2 = [1 alpha alpha^2 alpha^3 alpha^4] ;
+
+Cxo =  fila1*hl20.Po(:,3)*-1 ;
+
+Czo = fila1*hl20.Po(:,1)*-1 ;
+
+Cmo = fila1*hl20.Po(:,2) ; 
+
+Czde= fila2*hl20.Pde(:,1)*-1 ;
+
+Cmde= fila2*hl20.Pde(:,2) ;
+
+Cxde= fila2*hl20.Pde(:,3)*-1 ;
+
+Cmq = interp1(hl20.Cdamp_alpha,hl20.Cmq,alpha);
+
+Cno = interp2(hl20.Cn_alpha,hl20.Cn_beta,hl20.Cn',alpha,abs(betha)); %(3,:) fila3 - ":" todas las columnas
+%Cno = interp1(hl20.Cn_beta,hl20.Cn(3,:),betha);
+
+Clp = interp1(hl20.Cdamp_alpha,hl20.Clp,alpha);
+
+Clr = interp1(hl20.Cdamp_alpha,hl20.Clr,alpha);
+
+Cnp = interp1(hl20.Cdamp_alpha,hl20.Cnp,alpha);
+
+Cnr = interp1(hl20.Cdamp_alpha,hl20.Cnr,alpha);
+
+
+xp(1)= ((0.5*rho*vinf^2*sup*(Cxo + Cxde*de) - m*g*sin(y(7)))/m) - y(5)*y(3) + y(6)*y(2);
+xp(2)= ((0.5*rho*vinf^2*sup*(-0.01242*betha)+ m*g*cos(y(7))* sin(y(8)))/ m) - y(6)*y(1) + y(4)*y(3);
+xp(3)= ((0.5*rho*vinf^2*sup*(Czo + Czde*de) + m*g*cos(y(7))*cos(y(8)))/m) - y(4)*y(2)+y(5)*y(1);
+xp(4)= (0.5*rho*vinf^2*sup*b*(-0.00787*betha + Clp*((y(4)*b)/(2*V))+ Clr*((y(6)*b)/(2*V)))-y(5)*y(6)*(Iz-Iy))/Ix; 
+xp(5)= (0.5*rho*vinf^2*sup*b*(Cmo + Cmq*((y(5)*c)/2*V) + Cmde*de )-y(5)*y(6)*(Ix-Iz))/Iy ;
+xp(6)= (0.5*rho*vinf^2*sup*b*(Cno + Cnp*((y(4)*b)/(2*V)) + Cnr*((y(6)*b)/(2*V))) - y(4)*y(5)*(Iy-Ix))/Iz ;
+xp(7)= y(5)*cos(y(8))- y(6)*sin(y(8));
+xp(8)= y(4) + y(5)*sin(y(8))*tan(y(8)) + y(6)*cos(y(8))*tan(y(7)) ;
+
+xp = xp';
+    
+
+
+
+end
+
+
---
+++ run.m
@@ -0,0 +1,53 @@
+%% SECTION TITLE
+% DESCRIPTIVE TEXTs
+
+
+clear
+clc
+global rho 
+global sup
+global b
+global c 
+global g
+global m
+global de
+global Ix
+global Iy
+global Iz
+global vinf
+global V
+g = 9.81;
+rho = 1.04 ;  
+sup = hl20.sref; 
+b = hl20.bref ; % en metros
+c = hl20.cbar ; % en metros
+m = hl20.mass + hl20.fuel ;
+Ix = hl20.Ixx ;
+Iy = hl20.Iyy ;
+Iz = hl20.Izz ;
+vinf = 180;
+de = -10 ; %angulo en grados
+alphacero = 7 ; %angulo en grados
+bethacero = 5 ; %angulo en grados
+titacero = -12*pi/180 ; %angulo en radianes
+
+uo = vinf * cosd(alphacero) * cosd(bethacero);
+vo = vinf * sind(bethacero);
+wo = vinf * sind(alphacero) * cosd(bethacero);
+y0 = [ uo vo wo 0 0 0 titacero 0];
+t = [0 10];
+
+%% Cálculo por métodos numéricos
+
+[t,y] = ode45(@f,t,y0);
+
+%% Gráficos
+
+subplot (421), plot(t,y(:,1)), xlabel('t'), ylabel('u'), grid on
+subplot (422), plot(t,y(:,4)), xlabel('t'), ylabel('p'), grid on
+subplot (423), plot(t,y(:,2)), xlabel('t'), ylabel('v'), grid on
+subplot (424), plot(t,y(:,5)), xlabel('t'), ylabel('q'), grid on
+subplot (425), plot(t,y(:,3)), xlabel('t'), ylabel('w'), grid on
+subplot (426), plot(t,y(:,6)), xlabel('t'), ylabel('r'), grid on
+subplot (427), plot(t,y(:,7)), xlabel('t'), ylabel('θ'), grid on
+subplot (428), plot(t,y(:,8)), xlabel('t'), ylabel('φ'), grid on
